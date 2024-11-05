79473238c7c8f0af349b2b353c2955ad1c9eadc9
sambit <sambit@wimerasys.com>
2024-11-05 12:08:20 +0530

    3rd chnages

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 4000;
 
 connectDB();
 const server = http.createServer(app);
+app.get('/', (req, res)=>{
+     res.send('Hello, Indic!');
+})
 server.listen(port, () => {
      console.log(`Server is listening on port ${port} 💥💨`);
 });
